50a5f310d0905e3fb270074451e1d6dcd5d6f749
danielRyndych <danil9269063881@gmail.com>
2022-11-15 13:25:18 +0300

    lesson_1 change

--- src/main/java/Lesson_01/Task01.java
+++ src/main/java/Lesson_01/Task01.java
@@ -3,15 +3,16 @@ package Lesson_01;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Task01 {
     public static void main(String[] args) {
-        String inputData = "<client>(Какие то данные)<data>79991113344;test@yanasex.ru;Иванов Иван Иванович</data></client>";
-        System.out.println(masking(inputData));
+        System.out.println("\"" +
+                masking("<client>что то тут есть...<data>79991113344;danil1872@yandex.ru;Даниилов Даниил Даниилович</data></client>") + "\"");
     }
 
     public static String masking(String str) {
-        List<String> list = deleteByRegex(str);
+        List<String> list = separationString(str);
         if (list.size() == 1)
             return list.toString().replaceAll("\\[", "").replaceAll("]", "");
         StringBuilder stringBuilder = new StringBuilder();
@@ -29,23 +30,21 @@ public class Task01 {
         return deleteDubSymbol(stringBuilder.append(list.get(2)).toString());
     }
 
-    private static String deleteDubSymbol(String str){
+    private static String deleteDubSymbol(String str) {
         int numberChar = 0;
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ';')
+            if (str.charAt(i) == ';') {
                 numberChar = i;
+            }
         }
-        for (int i = 0; i < numberChar; i++) {
-            stringBuilder.append(str.charAt(i));
-        }
-        for (int i = numberChar; i < str.length()-1; i++) {
-            stringBuilder.append(str.charAt(i+1));
+        for (int i = 0; i < str.length() - 1; i++) {
+            stringBuilder.append(i < numberChar ? str.charAt(i) : str.charAt(i + 1));
         }
         return stringBuilder.toString();
     }
 
-    private static List<String> deleteByRegex(String str) {
+    private static List<String> separationString(String str) {
         String stringSplit = str.replaceAll("<client>.+<data>|<client><data>", "").replaceAll("</data>.+</client>|</data></client>", "");
         if (stringSplit.isEmpty()) {
             List<String> list = new ArrayList<>();
@@ -58,49 +57,28 @@ public class Task01 {
     }
 
     private static String maskingEmail(String str) {
-        StringBuilder stringBuilder = new StringBuilder();
-        boolean key = false;
-        for (int i = 0; i < str.length() - 1; i++) {
-            if (str.charAt(i) == '@') {
-                stringBuilder.append(str.charAt(i));
-            } else if (str.charAt(i + 1) == '@') {
-                stringBuilder.append("*");
-                key = true;
-            } else if (key) {
-                if (str.charAt(i) != '.') {
-                    stringBuilder.append("*");
-                } else {
-                    stringBuilder.append(str.charAt(i));
-                    key = false;
-                }
-            } else {
-                stringBuilder.append(str.charAt(i));
-            }
-        }
-        return stringBuilder.append(str.charAt(str.length() - 1)).append(";").toString();
+        return new StringBuilder().append(str)
+                .replace(str.indexOf("@") - 1, str.indexOf("@"), "*")
+                .replace(str.indexOf("@") + 1, str.indexOf("."), repeat('*', (str.indexOf(".") - str.indexOf("@")) - 1)).append(";").toString();
     }
 
     private static String maskingDataUser(String str) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder(), stringBuilderTemp = new StringBuilder();
         String[] strings = str.split(" ");
-        stringBuilder.append(strings[0]).append(" ");
-        for (int i = 1; i < strings.length; i++) {
-            if (i == 1) {
-                for (int j = 0; j < strings[1].length(); j++) {
-                    stringBuilder.append(j == 0 || j == strings[1].length() - 1 ? strings[1].charAt(j) : "*");
-                }
-            } else {
-                stringBuilder.append(" ").append(strings[2].charAt(0)).append(".;");
-            }
-        }
-        return stringBuilder.toString();
+        stringBuilder.append(strings[0])
+                .replace(1, strings[0].length() - 1, repeat('*', strings[0].length() - 2)).append(" ").append(strings[1]).append(" ");
+        stringBuilderTemp.append(strings[2]).replace(1, strings[2].length(), ".;");
+        return stringBuilder.append(stringBuilderTemp).toString();
     }
 
     private static String maskingNumber(String str) {
+        return new StringBuilder().append(str).replace(4, 7, repeat('*', 3)).append(";").toString();
+    }
+
+    //в моей jdk нету такого метода в реализации String, поэтому сделал свой
+    private static String repeat(char symbol, int repeat) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            stringBuilder.append((i >= 4 && i <= 6) ? "*" : str.charAt(i));
-        }
-        return stringBuilder.append(";").toString();
+        Stream.iterate(1, o -> o + 1).limit(repeat).forEach(o -> stringBuilder.append("*"));
+        return stringBuilder.toString();
     }
 }
